d134c609dfdc7e6004eeffaca6305fa716d0eca0
Mateusz Bernacik <mat.bernacik@gmail.com>
2023-03-02 22:29:25 +0100

    add listener to paste send anti-soy-chat message on new chat click

--- anti-soy-bot.js
+++ anti-soy-bot.js
@@ -1,3 +1,18 @@
+window.addEventListener('load', function() {
+  // Add an event listener for clicks on the page
+  document.addEventListener('click', function(e) {
+
+    // Check if the target of the click matches the first child of the "NAV" element
+    const target = e.target;
+    const navFirstChild = document.querySelector('nav :first-child');
+    if (target === navFirstChild) {
+
+      // Call the pasteText function to paste the message
+      pasteText();
+    }
+  });
+});
+
 function pasteText() {
   const chatInputSelector = 'textarea[data-id]';
   const chatInput = document.querySelector(chatInputSelector);
@@ -13,7 +28,7 @@ function pasteText() {
 function clickSendButton() {
   const chatInputSelector = 'textarea[data-id]';
   const chatInput = document.querySelector(chatInputSelector);
-  
+
   if (chatInput) {
     const sendButton = chatInput.nextElementSibling;
 
@@ -24,4 +39,3 @@ function clickSendButton() {
 }
 
 setTimeout(pasteText, 1000); // Adjust the timeout value as needed
-
